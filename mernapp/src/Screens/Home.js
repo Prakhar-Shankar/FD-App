b716068d3846e19a6224566f46f8b3ebbffa64d0
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-08-04 13:29:43 +0530

    Front-end structuring

--- mernapp/src/Screens/Home.js
+++ mernapp/src/Screens/Home.js
@@ -1,26 +1,18 @@
 import React from "react";
+
+
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import Card from "../Components/Card";
+import Carousel from "../Components/Carousel";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <div>
-        <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is some important text...
-            </p>
-            <div className="container w-100">
-                <select className="m-2 h-100 w-100 bg-success"></select>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
+      <div><Navbar /></div>
+      <div><Carousel /></div>
+      <div className="m-3"><Card /></div>
+      <div><Footer /></div>
     </div>
   );
 }
