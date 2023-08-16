6c8e1ed91196598d7f15000e84f6796dbb069987
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-08-16 21:58:07 +0530

    Express and node setup

--- mernapp/Backend/db.js
+++ mernapp/Backend/db.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb+srv://moodiefoodie:moodiefoodie123@cluster0.myx0z2p.mongodb.net/?retryWrites=true&w=majority'
 
-const mongoDB = () =>{
-    mongoose.connect(mongoURI, () => {
+const mongoDB = async() =>{
+   await mongoose.connect(mongoURI,{useNewUrlParser: true}, (err, result) => {
+    if(err) console.log("---", err)
+    else{
         console.log("Connected")
+    }
     });
 }
 
