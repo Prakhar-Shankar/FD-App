b7e2717b5c7e9e62de68243163d5903df84dc570
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-08-25 10:17:46 +0530

    Some minor fixes

--- mernapp/Backend/db.js
+++ mernapp/Backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://moodiefoodie:moodiefoodie123@cluster0.myx0z2p.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://moodiefoodie:moodiefoodie123@cluster0.myx0z2p.mongodb.net/moodiefoodie?retryWrites=true&w=majority'
 
 const mongoDB = async() =>{
-   await mongoose.connect(mongoURI,{useNewUrlParser: true}, (err, result) => {
+   await mongoose.connect(mongoURI,{useNewUrlParser: true},async (err, result) => {
     if(err) console.log("---", err)
     else{
         console.log("Connected")
