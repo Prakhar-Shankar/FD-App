51285a5cb131c99f58cb961ed8625cf77deafa91
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-08-25 10:44:33 +0530

    Issues commiting to have a record

--- mernapp/Backend/db.js
+++ mernapp/Backend/db.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://moodiefoodie:moodiefoodie123@cluster0.myx0z2p.mongodb.net/moodiefoodie?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://moodiefoodie:moodiefoodie123@cluster0.myx0z2p.mongodb.net/moodiefoodie-mern?retryWrites=true&w=majority'
 
-const mongoDB = async() =>{
-   await mongoose.connect(mongoURI,{useNewUrlParser: true},async (err, result) => {
-    if(err) console.log("---", err)
-    else{
-        console.log("Connected")
+const mongoDB = async () => {
+    try {
+      mongoose.set("strictQuery", false);
+      mongoose.connect(mongoURI);
+      console.log("Connected to Mongo Successfully!");
+      const fetched_data = await mongoose.connection.db.collection("food_item");
+      fetched_data.find({}).toArray(function(err, data){
+        if(err) console.log(err);
+        else console.log(data); 
+      })
+    } catch (error) {
+      console.log(error);
     }
-    });
-}
+  };
 
 module.exports = mongoDB;
